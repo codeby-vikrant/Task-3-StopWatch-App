7e350b13e55ca88438a602c1acbd42cf042279de
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-21 14:17:32 +0530

    made the start time functionality

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -19,14 +19,14 @@ class _HomeScreenState extends State<HomeScreen> {
   bool started = false;
   List laps = [];
 
-  void stop(){
+  void stop() {
     timer!.cancel();
     setState(() {
       started = false;
     });
   }
 
-  void reset(){
+  void reset() {
     timer!.cancel();
     setState(() {
       seconds = 0;
@@ -37,6 +37,40 @@ class _HomeScreenState extends State<HomeScreen> {
       digitMinutes = "00";
       digitHours = "00";
       started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void start() {
+    started = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+      if (localSeconds > 59) {
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        } else {
+          localMinutes++;
+          localSeconds = 0;
+        }
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+
+        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
+        digitHours = (hours >= 10) ? "$hours" : "0$hours";
+      });
     });
   }
 
