f9205daaf8c89d21e1a66fc13c9d864670045e65
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-21 14:24:01 +0530

    moved the functionality to another file

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:task_3_stopwatch_app/constants/colors.dart';
 import 'package:task_3_stopwatch_app/widgets/buttons.dart';
@@ -13,67 +11,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int seconds = 0, minutes = 0, hours = 0;
-  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
-  Timer? timer;
-  bool started = false;
-  List laps = [];
-
-  void stop() {
-    timer!.cancel();
-    setState(() {
-      started = false;
-    });
-  }
-
-  void reset() {
-    timer!.cancel();
-    setState(() {
-      seconds = 0;
-      minutes = 0;
-      hours = 0;
-
-      digitSeconds = "00";
-      digitMinutes = "00";
-      digitHours = "00";
-      started = false;
-    });
-  }
-
-  void addLaps() {
-    String lap = "$digitHours:$digitMinutes:$digitSeconds";
-    setState(() {
-      laps.add(lap);
-    });
-  }
-
-  void start() {
-    started = true;
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      int localSeconds = seconds + 1;
-      int localMinutes = minutes;
-      int localHours = hours;
-      if (localSeconds > 59) {
-        if (localMinutes > 59) {
-          localHours++;
-          localMinutes = 0;
-        } else {
-          localMinutes++;
-          localSeconds = 0;
-        }
-      }
-      setState(() {
-        seconds = localSeconds;
-        minutes = localMinutes;
-        hours = localHours;
-
-        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
-        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
-        digitHours = (hours >= 10) ? "$hours" : "0$hours";
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- lib/widgets/lapswidget.dart
+++ lib/widgets/lapswidget.dart
@@ -1,10 +1,78 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:task_3_stopwatch_app/constants/colors.dart';
 
-class LapsBox extends StatelessWidget {
+class LapsBox extends StatefulWidget {
   const LapsBox({
     super.key,
   });
+
+  @override
+  State<LapsBox> createState() => _LapsBoxState();
+}
+
+class _LapsBoxState extends State<LapsBox> {
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
+  Timer? timer;
+  bool started = false;
+  List laps = [];
+
+  void stop() {
+    timer!.cancel();
+    setState(() {
+      started = false;
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+
+      digitSeconds = "00";
+      digitMinutes = "00";
+      digitHours = "00";
+      started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void start() {
+    started = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+      if (localSeconds > 59) {
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        } else {
+          localMinutes++;
+          localSeconds = 0;
+        }
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+
+        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
+        digitHours = (hours >= 10) ? "$hours" : "0$hours";
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +80,12 @@ class LapsBox extends StatelessWidget {
       height: 400.0,
       decoration: BoxDecoration(
           color: swBoxColor, borderRadius: BorderRadius.circular(8.0)),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return const Row();
+        },
+        itemCount: laps.length,
+      ),
     );
   }
 }
